428cd62b6331394ed6c419b9401d674372e1aa6b
Vivek Sharma <161102201+geekovic@users.noreply.github.com>
2024-12-07 21:44:17 +0530

    Update Parenthesis Balancer.py

--- Parenthesis Balancer.py
+++ Parenthesis Balancer.py
@@ -1,4 +1,3 @@
-
 def is_balanced(s):
     #Function to check if a string of parentheses is balanced
     stack = []
@@ -18,14 +17,21 @@ def is_balanced(s):
 
 
 # Main Execution
-if __name__ == "__main__":
+if __name__== "__main__":
     print("Parentheses Balancer")
     while True:
+        flag = True
         string = input("\nEnter a string of parentheses (or type 'exit' to quit): ")
+        for char in string:
+            if char not in  "({[)]}":
+                flag = False
+                break
+        if flag == False and string!='exit' :
+            print("Enter Valid Parenthesis")
+            continue
         if string.lower() == 'exit':
             print("Exiting program. Goodbye!")
             break
-
         if is_balanced(string):
             print("The string is balanced.")
         else:
